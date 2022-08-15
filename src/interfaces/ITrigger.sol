28e3f30d988befff7f0af014d8a16f84f495ba5d
David Laprade <david.laprade@gmail.com>
2022-08-15 16:01:01 -0400

    chore: flatten ITrigger

--- src/interfaces/ITrigger.sol
+++ src/interfaces/ITrigger.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
-import "src/interfaces/ITriggerEvents.sol";
-
 /**
  * @dev The minimal functions a trigger must implement to work with the Cozy protocol.
  */
 interface ITrigger is ITriggerEvents {
+  /// @dev Emitted when a new set is added to the trigger's list of sets.
+  event SetAdded(ISet set);
+
+  /// @dev Emitted when a trigger's state is updated.
+  event TriggerStateUpdated(CState indexed state);
+
   /// @notice The current trigger state. This should never return PAUSED.
   function state() external returns(CState);
 
