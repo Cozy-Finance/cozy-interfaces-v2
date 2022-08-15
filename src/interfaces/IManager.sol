b051f933fd665c0dd6c3f072f4a0e245cc36cd8b
David Laprade <david.laprade@gmail.com>
2022-08-15 13:45:37 -0400

    feat: flatten manager interface

--- src/interfaces/IManager.sol
+++ src/interfaces/IManager.sol
@@ -1,10 +1,103 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
-import "src/interfaces/IManagerEvents.sol";
-import "src/interfaces/IGovernable.sol";
+import "src/interfaces/IConfig.sol";
+import "src/interfaces/ICState.sol";
+import "src/interfaces/IERC20.sol";
+import "src/interfaces/ISet.sol";
 
-interface IManager is IManagerEvents, IGovernable {
+interface IManager is ICState, IConfig {
+  /// @dev Emitted when a new set is given permission to pull funds from the backstop if it has a shortfall after a trigger.
+  event BackstopApprovalStatusUpdated(address indexed set, bool status);
+
+  /// @dev Emitted when the Cozy configuration delays are updated, and when a set is created.
+  event ConfigParamsUpdated(uint256 configUpdateDelay, uint256 configUpdateGracePeriod);
+
+  /// @dev Emitted when a Set admin's queued set and market configuration updates are applied, and when a set is created.
+  event ConfigUpdatesFinalized(address indexed set, SetConfig setConfig, MarketInfo[] marketInfos);
+
+  /// @dev Emitted when a Set admin queues new set and/or market configurations.
+  event ConfigUpdatesQueued(address indexed set, SetConfig setConfig, MarketInfo[] marketInfos, uint256 updateTime, uint256 updateDeadline);
+
+  /// @dev Emitted when accrued Cozy reserve fees and backstop fees are swept from a Set to the Cozy admin (for reserves) and backstop.
+  event CozyFeesClaimed(address indexed set);
+
+  /// @dev Emitted when the delays affecting user actions are initialized or updated by the Cozy admin.
+  event DelaysUpdated(uint256 minDepositDuration, uint256 withdrawDelay, uint256 purchaseDelay);
+
+  /// @dev Emitted when the deposit cap for an asset is updated by the Cozy admin.
+  event DepositCapUpdated(IERC20 indexed asset, uint256 depositCap);
+
+  /// @dev Emitted when the Cozy protocol fees are updated by the Cozy admin.
+  /// Changes to fees for the Set admin are emitted in ConfigUpdatesQueued and ConfigUpdatesFinalized.
+  event FeesUpdated(Fees fees);
+
+  /// @dev Emitted when a market, defined by it's trigger address, changes state.
+  event MarketStateUpdated(address indexed set, address indexed trigger, CState indexed state);
+
+  /// @dev Emitted when the owner address is updated.
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  /// @dev Emitted when the pauser address is updated.
+  event PauserUpdated(address indexed newPauser);
+
+  /// @dev Emitted when the admin of a set is updated.
+  event SetAdminUpdated(address indexed set, address indexed admin);
+
+  /// @dev Emitted when the Set admin claims their portion of fees.
+  event SetFeesClaimed(address indexed set, address _receiver);
+
+  /// @dev Emitted when the Set's pauser is updated.
+  event SetPauserUpdated(address indexed set, address indexed pauser);
+
+  /// @dev Emitted when the Set's state is updated.
+  event SetStateUpdated(address indexed set, CState indexed state);
+
+  /// @notice Used to update backstop approvals.
+  struct BackstopApproval {
+    ISet set;
+    bool status;
+  }
+
+  /// @notice All delays that can be set by the Cozy admin.
+  struct Delays {
+    uint256 configUpdateDelay; // Duration between when a set/market configuration updates are queued and when they can be executed.
+    uint256 configUpdateGracePeriod; // Defines how long the admin has to execute a configuration change, once it can be executed.
+    uint256 minDepositDuration; // The minimum duration before a withdrawal can be initiated after a deposit.
+    uint256 withdrawDelay; // If not paused, suppliers must queue a withdrawal and wait this long before completing the withdrawal.
+    uint256 purchaseDelay; // Protection does not mature (i.e. it cannot claim funds from a trigger) until this delay elapses after purchase.
+  }
+
+  /// @notice All fees that can be set by the Cozy admin.
+  struct Fees {
+    uint16 depositFeeReserves;  // Fee charged on deposit and min, allocated to the protocol reserves, denoted in zoc.
+    uint16 depositFeeBackstop; // Fee charged on deposit and min, allocated to the protocol backstop, denoted in zoc.
+    uint16 purchaseFeeReserves; // Fee charged on purchase, allocated to the protocol reserves, denoted in zoc.
+    uint16 purchaseFeeBackstop; // Fee charged on purchase, allocated to the protocol backstop, denoted in zoc.
+    uint16 cancellationFeeReserves; // Fee charged on cancellation, allocated to the protocol reserves, denoted in zoc.
+    uint16 cancellationFeeBackstop; // Fee charged on cancellation, allocated to the protocol backstop, denoted in zoc.
+  }
+
+  /// @notice A market or set is considered inactive when it's FROZEN or PAUSED.
+  struct InactivityData {
+    uint64 inactiveTransitionTime; // The timestamp the set/market transitioned from active to inactive, if currently inactive. 0 otherwise.
+    InactivePeriod[] periods; // Array of all inactive periods for a set or market.
+  }
+
+  /// @notice Set related data.
+  struct SetData {
+    // When a set is created, this is updated to true.
+    bool exists;
+     // If true, this set can use funds from the backstop.
+    bool approved;
+    // Earliest timestamp at which finalizeUpdateConfigs can be called to apply config updates queued by updateConfigs.
+    uint64 configUpdateTime;
+    // Maps from set address to the latest timestamp after configUpdateTime at which finalizeUpdateConfigs can be
+    // called to apply config updates queued by updateConfigs. After this timestamp, the queued config updates
+    // expire and can no longer be applied.
+    uint64 configUpdateDeadline;
+  }
+
   /// @notice Max fee for deposit and purchase.
   function MAX_FEE() view external returns (uint256);
 
